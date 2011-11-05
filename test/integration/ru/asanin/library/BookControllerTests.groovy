e5895dcde16c95cb170135e80a3ffd7a2b9b1098
alex sanin <vozhdb@gmail.com>
2011-11-05 15:44:15 +0400

    Tests for new model

--- test/integration/ru/asanin/library/BookControllerTests.groovy
+++ test/integration/ru/asanin/library/BookControllerTests.groovy
@@ -14,52 +14,47 @@ class BookControllerTests extends GroovyTestCase {
         def doc = new Document(description: "mustache").save()
         new Reader(fio: "uncle Fedor", document: new DocumentInstance(document: doc, details: "some")).save()
         def author = new Author(fio: "Eduard Uspenskiy").save()
-        def book = new Book(title: BOOK_TITLE, authors: [author])
-        for(int i = 0; i < BOOK_INSTANCE_SIZE; i++){
-            book.addToInstances(new BookInstance(code: i.toString()))
-        }
-        book.save()
+        new Book(title: BOOK_TITLE, authors: [author], number: BOOK_INSTANCE_SIZE).save()
     }
 
     void testReleaseEmpty() {
-        def origBooks = BookInstance.findAllByReaderIsNull()
         controller.release()
-        assertEquals(400 , controller.response.status)
-        assertEquals(origBooks , BookInstance.findAllByReaderIsNull())
+        assertStatus(400)
     }
 
     void testRelease() {
+        def book = Book.findByTitle(BOOK_TITLE)
+        def reader = Reader.findAll().get(0)
         for(int i = 0; i < BOOK_INSTANCE_SIZE; i++){
-            takeBook(BOOK_TITLE, Reader.findAll().get(0).id)
+            takeBook(BOOK_TITLE, reader.id)
         }
-        def bookInstanceList = BookInstance.list()
+
         for(int i = 0; i < BOOK_INSTANCE_SIZE; i++){
-            releaseBook(bookInstanceList.get(i).code)
+            releaseBook(BOOK_TITLE, reader.id)
             assertStatus(200)
-            assertEquals(i + 1, BookInstance.findAllByReaderIsNull().size())
+            assertEquals(BOOK_INSTANCE_SIZE - i - 1, book.readers.size())
+            assertEquals(BOOK_INSTANCE_SIZE - i - 1, reader.books.size())
         }
-        releaseBook(bookInstanceList.get(0).code)
+        releaseBook(BOOK_TITLE, reader.id)
         assertStatus(400)
-        assertEquals(BOOK_INSTANCE_SIZE, BookInstance.findAllByReaderIsNull().size())
     }
 
     void testTakeEmpty(){
-        def origBooks = BookInstance.findAllByReaderIsNull()
         controller.take()
         assertStatus(400)
-        assertEquals(origBooks , BookInstance.findAllByReaderIsNull())
     }
 
     void testTake(){
-        Long readerId = Reader.findAll().get(0).id
+        def reader = Reader.findAll().get(0)
+        def book = Book.findByTitle(BOOK_TITLE)
         for(int i = 0; i < BOOK_INSTANCE_SIZE; i++){
-            takeBook(BOOK_TITLE, readerId)
+            takeBook(BOOK_TITLE, reader.id)
             assertStatus(200)
-            assertEquals(BOOK_INSTANCE_SIZE - i - 1, BookInstance.findAllByReaderIsNull().size())
+            assertEquals(i + 1, reader.books.size())
+            assertEquals(i + 1, book.readers.size())
         }
-        takeBook(BOOK_TITLE, readerId)
+        takeBook(BOOK_TITLE, reader.id)
         assertStatus(400)
-        assertEquals(0, BookInstance.findAllByReaderIsNull().size())
     }
 
     private void takeBook(String title, Long readerId){
@@ -68,8 +63,8 @@ class BookControllerTests extends GroovyTestCase {
         controller.params.clear()
     }
 
-    private void releaseBook(String code){
-        controller.params.put("code", code)
+    private void releaseBook(String title, Long readerId){
+        controller.params.putAll([title : title, readerId: readerId])
         controller.release()
         controller.params.clear()
     }
